1007680363c6bf36bf69b5d0f90ba5d1e249100a
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2009-07-14 15:52:49 -0400

    add encoding

--- lottocheck.py
+++ lottocheck.py
@@ -23,7 +23,7 @@ for line in data:
 matches1 = compare.getMatching(picks, winning1)
 matches2 = compare.getMatching(picks, winning2)
 
-header = """Content-type: text/html
+header = """Content-type: text/html; charset=UTF-8
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
